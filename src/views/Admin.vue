7327a2b2fa1c4c70c5a1f49ea0ed5b8606ab7837
123456pan <wang1528766955@qq.com>
2025-10-16 12:55:07 +0800

    feat: add Admin page component

---
+++ src/views/Admin.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>管理员页面</h1>
+    <p>这是管理员后台页面</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AdminPage'
+}
+</script>
+
+<style scoped>
+/* 可以添加一些基本样式 */
+</style>
